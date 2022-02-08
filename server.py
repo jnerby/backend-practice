63e01a8651025929888cdcb381cf9ee24aeb7548
Jen Nerby <nerby.jennifer@gmail.com>
2022-02-08 08:56:28 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,6 @@
+
+
+"""PART 1 - GET route that returns all recipe names"""
+"""PART 2 - GET route that takes in recipe name as a string and returns ingredients and # of steps"""
+"""PART 3 - POST route to add additional recipes to data.json"""
+"""PART 4 - PUT route to update existing records"""
